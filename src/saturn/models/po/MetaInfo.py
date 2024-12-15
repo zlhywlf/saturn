7adead8246354f5af408501b6935e5efe60289cd
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-12-15 22:03:06 +0800

    optimize

--- src/saturn/models/po/MetaInfo.py
+++ src/saturn/models/po/MetaInfo.py
@@ -15,12 +15,22 @@ class MetaInfo(BaseTable):
 
     __tablename__ = "meta"
 
-    pid: Mapped[int] = mapped_column(ForeignKey("meta.id"), nullable=True)
+    pid: Mapped[int | None] = mapped_column(ForeignKey("meta.id"), nullable=True)
+    sid: Mapped[int | None] = mapped_column(ForeignKey("meta.id"), nullable=True)
     tid: Mapped[int] = mapped_column(ForeignKey("task.id"), nullable=True)
     name: Mapped[str] = mapped_column(String())
     type: Mapped[int] = mapped_column(Integer(), default=0)
-    meta: Mapped[list["MetaInfo"]] = relationship(lazy="immediate")
     config: Mapped[str | None] = mapped_column(String(), nullable=True)
+    meta: Mapped[list["MetaInfo"]] = relationship(
+        lazy="immediate",
+        primaryjoin=lambda: (MetaInfo.pid == MetaInfo.id),
+    )
+    sub: Mapped["MetaInfo"] = relationship(
+        lazy="immediate",
+        primaryjoin=lambda: (MetaInfo.sid == MetaInfo.id),
+        remote_side=[sid],
+        uselist=False,
+    )
 
     @classmethod
     def load_meta(cls, meta_info: "MetaInfo") -> Meta:
@@ -29,6 +39,7 @@ class MetaInfo(BaseTable):
             id=meta_info.id,
             name=meta_info.name,
             type=meta_info.type,
-            meta=[cls.load_meta(_) for _ in meta_info.meta],
+            meta=[cls.load_meta(_) for _ in meta_info.meta] if meta_info.meta else None,
             config=meta_info.config or "",
+            sub=cls.load_meta(meta_info.sub) if meta_info.sub else None,
         )
