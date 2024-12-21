85f0ac2ba3d0202e1d6c81f6048101f950c9ddf1
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-12-21 21:13:17 +0800

    optimize

--- src/saturn/core/decisions/nodes/ListPageDecisionNode.py
+++ src/saturn/core/decisions/nodes/ListPageDecisionNode.py
@@ -31,6 +31,7 @@ class ListPageDecisionNode(DecisionNode):
         """Init."""
         self._pattern = re.compile(r"\b\d+\b")
         self._full_url_pattern = re.compile(r'https?://(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?:/[^"\s]*)?')
+        self._relative_url_pattern = re.compile(r'(?:/[^\s"\'<>]+|\.+/[^\s"\'<>]+|\.\./[^\s"\'<>]+)')
 
     @override
     async def handle(self, ctx: Context) -> AsyncGenerator[Result | Task, None]:
@@ -49,7 +50,7 @@ class ListPageDecisionNode(DecisionNode):
             url = None
             if selector.root.tag == "a":
                 url = await self._handle_a(config, selector)
-            if selector.root.tag == "div":
+            if selector.root.tag == "div" or selector.root.tag == "li":
                 url = await self._handle_div(selector)
             if url and next_meta:
                 yield Task(
@@ -65,16 +66,21 @@ class ListPageDecisionNode(DecisionNode):
 
     async def _handle_a(self, config: Config, selector: Selector) -> str | None:
         if href := selector.attrib.get("href"):
-            return await self._handle_a_javascript(config, selector) if href.startswith(("javascript:", "#")) else href
+            return (
+                await self._handle_a_javascript(config, selector)
+                if href.startswith(("javaScript:", "javascript:", "#"))
+                else href
+            )
         return await self._handle_a_javascript(config, selector)
 
     async def _handle_a_javascript(self, config: Config, selector: Selector) -> str | None:
-        match = self._pattern.search(selector.xpath(config.page_path).extract_first() or "")
-        if not match:
-            return None
-        return config.query % {"page": match.group()}
+        if match := self._pattern.search(selector.xpath(config.page_path).extract_first() or ""):
+            return config.query % {"page": match.group()}
+        return None
 
     async def _handle_div(self, selector: Selector) -> str | None:
         if match := self._full_url_pattern.search(selector.attrib.get("onclick") or ""):
+            return match.group()
+        if match := self._relative_url_pattern.search(selector.attrib.get("onclick") or ""):
             return match.group()
         return None
