08d921aba64ee12f263fb92ea21719dbf12505b7
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-12-24 17:42:42 +0800

    optimize

--- src/saturn/frameworks/scrapy/pipelines/SaveResultLocal.py
+++ src/saturn/frameworks/scrapy/pipelines/SaveResultLocal.py
@@ -21,7 +21,13 @@ class SaveResultLocal:
         spider.log(f"{item.type!s},{len(item.content or '')}")
         loop = asyncio.get_event_loop()
         file_name = (
-            item.name.replace("/", "_").replace(":", "_").replace("?", "_").replace("\n", "").replace(" ", "")
+            item.name.replace("/", "_")
+            .replace(":", "_")
+            .replace("?", "_")
+            .replace("\n", "")
+            .replace(" ", "")
+            .replace("\r","")
+            .replace("\t","")
             if item.name
             else f"{len(item.content or '')}"
         )
